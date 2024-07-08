76c18c0c1c086b33a1380f45114e3d9a84cd94bf
YashO238 <www.yyaasshh@gmail.com>
2024-07-08 18:32:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,57 @@
+body {
+    font-family: Arial, sans-serif;
+    background-color: #121212;
+    color: #ffffff;
+    text-align: center;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #03dac6;
+    padding: 10px 0;
+}
+
+header h1 {
+    margin: 0;
+}
+
+main {
+    padding: 20px;
+}
+
+.search {
+    margin: 20px 0;
+}
+
+.search input {
+    padding: 10px;
+    width: 200px;
+    margin-right: 10px;
+}
+
+.search button {
+    padding: 10px;
+    background-color: #03dac6;
+    border: none;
+    color: #121212;
+    cursor: pointer;
+}
+
+.search button:hover {
+    background-color: #029e8a;
+}
+
+.weather-info {
+    margin-top: 20px;
+}
+
+.weather-info h2 {
+    margin: 10px 0;
+}
+
+footer {
+    background-color: #03dac6;
+    padding: 10px 0;
+    margin-top: 20px;
+}
